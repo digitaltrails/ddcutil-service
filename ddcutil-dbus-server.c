3d15a87cd24acfa5eb706666390696d33d813834
digitaltrails <michael@actrix.gen.nz>
2023-11-14 12:06:12 +1300

    Add copyright.

--- ddcutil-dbus-server.c
+++ ddcutil-dbus-server.c
@@ -9,7 +9,35 @@
 #include <string.h>
 
 /* ----------------------------------------------------------------------------------------------------
+ * ddcutil-dbus-server.c
+ * ---------------------
+ * A D-Bus server for libddcutil/ddcutil
+ *
+ *
+ * Copyright (C) 2023, Michael Hamilton
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/*
  * Code structure based on https://github.com/bratsche/glib/blob/master/gio/tests/gdbus-example-server.c
+ *
+ * Info on D-Bus:     https://dbus.freedesktop.org/doc/dbus-specification.html
+ * Info on GDbus:     https://docs.gtk.org/glib/
+ * Info on lbddcutil: https://www.ddcutil.com/api_main/ and the inline documentation the header files at
+ *                    https://github.com/rockowitz/ddcutil/tree/2.0.2-dev/src/public
  */
 
 static GDBusNodeInfo *introspection_data = NULL;
