6e6e2639f039d439a786d346519b45db595e91cc
digitaltrails <michael@actrix.gen.nz>
2023-11-24 13:11:34 +1300

    Signal client example.

---
+++ examples/ddcutil-dbus-signal-receiver.py
@@ -0,0 +1,49 @@
+# ddcutil-dbus-client.py
+# ----------------------
+# Python dasbus example of receiving signals from ddcutil-dbus-server
+#
+# Copyright (C) 2023, Michael Hamilton
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+import base64
+import time
+# dasbus seems to be recent, popular and actively supported.
+# (perhaps also look at dbus-next)
+
+from collections import namedtuple
+
+from dasbus.connection import SessionMessageBus
+from dasbus.loop import EventLoop
+
+bus = SessionMessageBus()
+
+ddcutil_proxy = bus.get_proxy(
+    "com.ddcutil.DdcutilService",  # The bus name
+    "/com/ddcutil/DdcutilObject",  # The object name
+)
+
+def callback(count: int, flags: int):
+    print("ConnectedDisplaysChanged Callback called", count, flags)
+
+print(f"{ddcutil_proxy.InterfaceVersionString=}")
+
+# Wait on signal
+ddcutil_proxy.ConnectedDisplaysChanged.connect(callback)
+
+loop = EventLoop()
+loop.run()
+
+
