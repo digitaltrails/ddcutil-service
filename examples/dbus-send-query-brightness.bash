c363ffa8d68c30b19e120d23d8bdeb0f5cf6e091
digitaltrails <michael@actrix.gen.nz>
2024-02-05 07:57:03 +1300

    Add more examples to the man page and examples directory.

--- examples/dbus-send-query-brightness.bash
+++ examples/dbus-send-query-brightness.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Example 'ddcutil getvcp 1 10' where display is 1 and the VCP-Code is 10 hex (0x10).
-dbus-send --dest=com.ddcutil.DdcutilService --print-reply --type=method_call /com/ddcutil/DdcutilObject  com.ddcutil.DdcutilInterface.GetVcp int32:1 string: byte:16 uint32:0
+dbus-send --dest=com.ddcutil.DdcutilService --print-reply --type=method_call \
+  /com/ddcutil/DdcutilObject \
+  com.ddcutil.DdcutilInterface.GetVcp int32:1 string: byte:16 uint32:0
