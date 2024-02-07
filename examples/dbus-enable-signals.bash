b4cca9aa97dbe0e54fee3d90d76d5513feaf9ab4
digitaltrails <michael@actrix.gen.nz>
2024-02-08 08:46:14 +1300

    Rationalise signal controls around --emit-signals and ServiceEmitSignals.  Default to off as it may often not be needed.

--- examples/dbus-enable-signals.bash
+++ examples/dbus-enable-signals.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Turn off change signalling
-echo "INFO: Optionally pass true or false as first script argument"
+echo "INFO: Optionally pass 'true' or 'false' as first script argument"
 set -x
 echo Setting property:
 dbus-send --session --dest=com.ddcutil.DdcutilService --print-reply \
    --type=method_call /com/ddcutil/DdcutilObject org.freedesktop.DBus.Properties.Set \
    string:com.ddcutil.DdcutilInterface \
-   string:ServiceMuteSignals variant:boolean:${1:-true}
+   string:ServiceEmitSignals variant:boolean:${1:-true}
 echo Getting property:
 dbus-send --session --dest=com.ddcutil.DdcutilService --print-reply \
    --type=method_call /com/ddcutil/DdcutilObject org.freedesktop.DBus.Properties.Get \
    string:com.ddcutil.DdcutilInterface \
-   string:ServiceMuteSignals
+   string:ServiceEmitSignals
 
